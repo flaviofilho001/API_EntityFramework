4563db15176aea4acd76f89d1c667897cf948ff1
Flavio Mesquita Marinho Filho <flaviomesquitamarinho13@gmail.com>
2024-05-11 23:20:09 -0300

    ADicionado README

--- obj/Debug/net8.0/ModuloAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ModuloAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53d0b1172659096eeae24b7d7325ae7c7dc57a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
